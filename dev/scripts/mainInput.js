8c5153ca2583a95c01967e7d42e3d31ff10a6df9
Justine Nguyen <justinexnguyen@gmail.com>
2017-11-28 18:03:35 -0500

    mainInput Component Updated

--- dev/scripts/mainInput.js
+++ dev/scripts/mainInput.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Qs from 'qs';
 
+const key = 'b7b40e21fdccd7460635c749a5dbb44b';
+const id = 'a31a2791';
+
 class MainInput extends React.Component {
    constructor() {
       super();
@@ -12,11 +15,10 @@ class MainInput extends React.Component {
       this.submitWord = this.submitWord.bind(this);
       this.handleChange = this.handleChange.bind(this);
       this.verifyFirstWord = this.verifyFirstWord.bind(this);
+      this.checkDefinition = this.checkDefinition.bind(this);
    }
     verifyFirstWord(word) {
         console.log(word);
-        const key = 'b7b40e21fdccd7460635c749a5dbb44b';
-        const id = 'a31a2791';
         axios({
             method: 'GET',
             url: 'https://proxy.hackeryou.com',
@@ -25,7 +27,7 @@ class MainInput extends React.Component {
                 return Qs.stringify(params, { arrayFormat: 'brackets' })
             },
             params: {
-                reqUrl: `https://od-api.oxforddictionaries.com/api/v1/en/${word}`,
+                reqUrl: `https://od-api.oxforddictionaries.com/api/v1/inflections/en/${word}`,
                 proxyHeaders: {
                     'header_params': 'value',
                     'app_key': key,
@@ -34,13 +36,51 @@ class MainInput extends React.Component {
                 xmlToJSON: false
             }
         }).then((res) => {
-            console.log(res);
+            // this is a real word
+            
+            const checkWord = res.data.results[0].id;
+            console.log(checkWord);
+            this.checkDefinition(checkWord)
+                
+
+        }).catch((error) => {
+            console.log(error.response);
+            // POPUP MODULE TO SAY ERRORORORORO
         });
     }
-   submitWord(e) {
-      e.preventDefault();
-      console.log(this.state.firstWord)
-      const submittedWord = this.state.firstWord
+
+
+    checkDefinition(word) {
+        console.log(word)
+        axios({
+            method: 'GET',
+            url: 'https://proxy.hackeryou.com',
+            dataResponse: 'json',
+            paramsSerializer: function (params) {
+                return Qs.stringify(params, { arrayFormat: 'brackets' })
+            },
+            params: {
+                reqUrl: `https://od-api.oxforddictionaries.com/api/v1/entries/en/${word}`,
+                proxyHeaders: {
+                    'header_params': 'value',
+                    'app_key': key,
+                    'app_id': id
+                },
+                xmlToJSON: false
+            }
+        }).then((res) => {
+            console.log(res)
+            let definition = res.data.results[0].lexicalEntries[0].entries[0].senses[0]
+            console.log(definition)
+        })
+    }
+
+
+
+    submitWord(e) {
+        e.preventDefault();
+        console.log(this.state.firstWord)
+        const submittedWord = this.state.firstWord
     //   Write a method to run axios to validate word from API when the button is submitted
     // If the word is true, then definition method runs. If not a word, 'try again'
     this.verifyFirstWord(submittedWord);
