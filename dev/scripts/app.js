13523c741f1b6f786e4c17d97c3a020068325d22
Justine Nguyen <justinexnguyen@gmail.com>
2017-11-29 17:10:40 -0500

    refactoring functions

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainInput from './MainInput';
-import FirstWordValidator from './firstWordValidator';
 import ResultsDisplay from './resultsDisplay';
 
 class App extends React.Component {
--- dev/scripts/mainInput.js
+++ dev/scripts/mainInput.js
@@ -7,29 +7,33 @@ const key = 'b7b40e21fdccd7460635c749a5dbb44b';
 const id = 'a31a2791';
 const definitionURL = 'entries';
 const wordURL = 'inflections';
+// let definition = res.data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
 
 class MainInput extends React.Component {
-   constructor() {
-      super();
-      this.state = {
+	constructor() {
+	super();
+		this.state = {
 			firstWord : '',
-			firstWordDefinition : '',
+			firstDefinition : '',
 			flippedWord : '',
-			flippedWordDefinition : ''
-      }
-      this.submitWord = this.submitWord.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.verifyFirstWord = this.verifyFirstWord.bind(this);
-		this.checkDefinition = this.checkDefinition.bind(this);
+			secondDefinition : ''
+		}
+		this.submitWord = this.submitWord.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.verifyWord = this.verifyWord.bind(this);
 		this.flipWord = this.flipWord.bind(this);
-		this.runRequest = this.runRequest.bind(this);
-		this.flipTheWord = this.flipTheWord.bind(this);
-		this.runApp = this.runApp.bind(this);
-		this.definitionRequest = this.definitionRequest.bind(this);
+		this.getDefinition = this.getDefinition.bind(this);
 	}
-	
+
+	handleChange(e) {
+		const wordSubmitted = e.target.value
+		this.setState({
+			firstWord: wordSubmitted
+		})
+	}
+
 	runRequest(urlSection, word) {
-		axios({
+		return axios({
 			method: 'GET',
 			url: 'https://proxy.hackeryou.com',
 			dataResponse: 'json',
@@ -46,151 +50,57 @@ class MainInput extends React.Component {
 				xmlToJSON: false
 			}
 		}).then ((result) => {
-			console.log(result);
 			return result
 		}).catch((error) => {
 				console.log(error.response);
 			// 	// POPUP MODULE TO SAY "word is not valid. Try again"
 		});
 	}
-	// Write a function to run the word functions on submit
-	runApp(wordURL, word, definitionURL) {
-		this.verifyFirstWord(wordURL, word);
-		const wordflip = this.flipTheWord(word);
-		console.log(wordflip);
-		this.verifyFlippedWord(wordURL, wordflip);
-		const firstDefinition = this.definitionRequest(definitionURL, word);
-		console.log(firstDefinition);
-	}
+
 	// first API request to check if the submitted word is valid or not
-	verifyFirstWord(wordURL, word) {
-		this.runRequest(wordURL, word);
-	}	
-
-	// If the first word is valid, then flip the word.
-	flipTheWord(word) {
-		const flippedWord = this.flipWord(word);
-		console.log(flippedWord);
-		return flippedWord
+	verifyWord(wordURL, word) {
+		return this.runRequest(wordURL, word);
 	}
 
-	// Check the flipped word is valid
-	verifyFlippedWord(wordURL, flippedWord) {
-		this.runRequest(wordURL, flippedWord);
-	}
-
-	// If the flipped word is valid, get the definition of the first and the flipped word
-	definitionRequest(definitionURL, wordToDefine) {
-		const define = this.runRequest(definitionURL, wordToDefine);
-		console.log(define);
-		return define;
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// .then((res) => {
-	// 	// this is a real word
-	// 	const checkWord = res.data.results[0].id;
-	// 	const firstDefinition = this.checkDefinition(checkWord);
-	// 	// const wordFlipped = this.flipWord(checkWord);
-	// 	// const flippedDefinition = this.checkDefinition(wordFlipped);
-	// 	this.setState ({
-	// 		firstWordDefinition : firstDefinition,
-	// 		// flippedWord : wordFlipped,
-	// 		// flippedWordDefinition : flippedDefinition
-	// 	})
-	// }).catch((error) => {
-	// 	console.log(error.response);
-	// 	// POPUP MODULE TO SAY "word is not valid. Try again"
-	// });
-	// else statement?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//second API request to check for the definition of the first word
-	checkDefinition(word) {
-		axios({
-			method: 'GET',
-			url: 'https://proxy.hackeryou.com',
-			dataResponse: 'json',
-			paramsSerializer: function (params) {
-				return Qs.stringify(params, { arrayFormat: 'brackets' })
-			},
-			params: {
-				reqUrl: `https://od-api.oxforddictionaries.com/api/v1/entries/en/${word}`,
-				proxyHeaders: {
-				'header_params': 'value',
-				'app_key': key,
-				'app_id': id
-				},
-				xmlToJSON: false
-			}
-		}).then((res) => {
-			// let definition = res.data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
-			// console.log(definition)
-			// this.setState ({
-			// 	firstWordDefinition : definition
-			// })
-		})
+	// get definition function to run request
+	getDefinition(definitionURL, word) {
+		return this.runRequest(definitionURL, word);
 	}
 
 	//write a function that takes the firstWord and flip it
-  flipWord(str) {
-	var splitString = str.split("");
-	var reverseArray = splitString.reverse();
-	var flippedWord = reverseArray.join("");
-	return flippedWord;
-  }
-
-  	//run first API call again to check if flippedWord is valid
+	flipWord(str) {
+		var splitString = str.split("");
+		var reverseArray = splitString.reverse();
+		var flippedWord = reverseArray.join("");
+		this.setState({
+			flippedWord
+		})
+	}
 
 	submitWord(e) {
 		e.preventDefault();
+		//Get the word that was typed in
 		console.log(this.state.firstWord)
-		const submittedWord = this.state.firstWord
-		this.runApp(wordURL, submittedWord, definitionURL);
-	}
-	
-   handleChange(e) {
-      const wordSubmitted = e.target.value
-      this.setState({
-         firstWord : wordSubmitted
-      })
 
+		//check if it is a real word
+		this.verifyWord(wordURL, this.state.firstWord)
+		
+		// flip the word
+		this.flipWord(this.state.firstWord)
+		console.log(this.state.flippedWord)
+
+		// verify flipped word
+		this.verifyWord(wordURL,this.state.flippedWord)
+
+		// get definitions for BOTH words
+		this.getDefinition(definitionURL, this.state.firstWord).then((data) => {
+			console.log(data)
+		})
 	}
-	
-   render() {
-      return (
-         <div>
+
+	render() {
+		return (
+		<div>
 			<form action="" 
 			onSubmit={this.submitWord}>
 
@@ -205,9 +115,9 @@ class MainInput extends React.Component {
 			<h2>{this.state.firstWordDefinition}</h2>
 			<h3>{this.state.flippedWord}</h3>
 			<h3>{this.state.secondWordDefinition}</h3>
-         </div>
-      )
-   }
+			</div>
+		)
+	}
 }
 
 
