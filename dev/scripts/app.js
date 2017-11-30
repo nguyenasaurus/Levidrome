5dfabaa6333359a1bbfe6dd37e5c7ed318310cae
Manish Sabharwal <manish57200@gmail.com>
2017-11-30 11:42:28 -0500

    added Header.js and Footer.js

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainInput from './MainInput';
 import ResultsDisplay from './resultsDisplay';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Header from 'header';
+
 
 class App extends React.Component {
     render() {
       return (
         <div>
+          <Header />
           <MainInput />
         </div>
       )
