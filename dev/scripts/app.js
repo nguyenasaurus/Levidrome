468e1e7545c31edf882230e06469d90ae41c9769
Manish Sabharwal <manish57200@gmail.com>
2017-11-30 17:33:27 -0500

    added firebase framework

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LevidromeValidator from './levidromevalidator';
 import ResultsDisplay from './resultsDisplay';
-
-
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import List from './list';
 import About from './about';
