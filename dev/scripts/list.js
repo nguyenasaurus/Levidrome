6dc0579fc4f514ad89ba0805f4e154c224a51865
Justine Nguyen <justinexnguyen@gmail.com>
2017-12-04 16:33:50 -0500

    updates to list.js

--- dev/scripts/list.js
+++ dev/scripts/list.js
@@ -54,6 +54,7 @@ class List extends React.Component {
         return (
             <div className="row">
                 <div className="wrapper">
+                <h2>List of Levidromes</h2>
                     <ul className="levidromeList">
                         {this.state.uniquePairs.map((pair) => {
                         return (
